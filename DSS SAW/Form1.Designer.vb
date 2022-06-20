34d1d1decadd25bb1318c2f4ec34ff47f42b89f9
Ikbal <Ikbal@AZUSANAKANO>
2022-06-20 10:20:16 +0700

    Add project files.

---
+++ DSS SAW/Form1.Designer.vb
@@ -0,0 +1,270 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txnama = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txnik = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtanggungan = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txpenghasilan = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txpekerjaan = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txstatus = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.txkondisi = New System.Windows.Forms.TextBox()
+        Me.txusia = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 110)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(55, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "NAMA"
+        '
+        'txnama
+        '
+        Me.txnama.Location = New System.Drawing.Point(148, 110)
+        Me.txnama.Name = "txnama"
+        Me.txnama.Size = New System.Drawing.Size(162, 20)
+        Me.txnama.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(12, 65)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(35, 20)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "NIK"
+        '
+        'txnik
+        '
+        Me.txnik.Location = New System.Drawing.Point(148, 65)
+        Me.txnik.Name = "txnik"
+        Me.txnik.Size = New System.Drawing.Size(162, 20)
+        Me.txnik.TabIndex = 1
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(12, 208)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(124, 20)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "TANGGUNGAN"
+        '
+        'txtanggungan
+        '
+        Me.txtanggungan.Location = New System.Drawing.Point(148, 208)
+        Me.txtanggungan.Name = "txtanggungan"
+        Me.txtanggungan.Size = New System.Drawing.Size(162, 20)
+        Me.txtanggungan.TabIndex = 1
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(12, 163)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(128, 20)
+        Me.Label4.TabIndex = 0
+        Me.Label4.Text = "PENGHASILAN "
+        '
+        'txpenghasilan
+        '
+        Me.txpenghasilan.Location = New System.Drawing.Point(148, 163)
+        Me.txpenghasilan.Name = "txpenghasilan"
+        Me.txpenghasilan.Size = New System.Drawing.Size(162, 20)
+        Me.txpenghasilan.TabIndex = 1
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(12, 303)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(104, 20)
+        Me.Label5.TabIndex = 0
+        Me.Label5.Text = "PEKERJAAN"
+        '
+        'txpekerjaan
+        '
+        Me.txpekerjaan.Location = New System.Drawing.Point(148, 303)
+        Me.txpekerjaan.Name = "txpekerjaan"
+        Me.txpekerjaan.Size = New System.Drawing.Size(162, 20)
+        Me.txpekerjaan.TabIndex = 1
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(12, 401)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(72, 20)
+        Me.Label6.TabIndex = 0
+        Me.Label6.Text = "STATUS"
+        '
+        'txstatus
+        '
+        Me.txstatus.Location = New System.Drawing.Point(148, 401)
+        Me.txstatus.Name = "txstatus"
+        Me.txstatus.Size = New System.Drawing.Size(162, 20)
+        Me.txstatus.TabIndex = 1
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(12, 258)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(116, 20)
+        Me.Label7.TabIndex = 0
+        Me.Label7.Text = "KONDISI RMH"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(12, 356)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(48, 20)
+        Me.Label8.TabIndex = 0
+        Me.Label8.Text = "USIA"
+        '
+        'txkondisi
+        '
+        Me.txkondisi.Location = New System.Drawing.Point(148, 258)
+        Me.txkondisi.Name = "txkondisi"
+        Me.txkondisi.Size = New System.Drawing.Size(162, 20)
+        Me.txkondisi.TabIndex = 1
+        '
+        'txusia
+        '
+        Me.txusia.Location = New System.Drawing.Point(148, 356)
+        Me.txusia.Name = "txusia"
+        Me.txusia.Size = New System.Drawing.Size(162, 20)
+        Me.txusia.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(40, 463)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(96, 55)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "INPUT DATA"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(521, 65)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(96, 55)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "LIHAT DATA"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(643, 173)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(96, 55)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "KONEKSI"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(643, 65)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(96, 55)
+        Me.Button4.TabIndex = 2
+        Me.Button4.Text = "URUTAN SAW"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(811, 599)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txusia)
+        Me.Controls.Add(Me.txpenghasilan)
+        Me.Controls.Add(Me.txkondisi)
+        Me.Controls.Add(Me.txnik)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txstatus)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.txtanggungan)
+        Me.Controls.Add(Me.txpekerjaan)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txnama)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txnama As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txnik As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents txtanggungan As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents txpenghasilan As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents txpekerjaan As System.Windows.Forms.TextBox
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents txstatus As System.Windows.Forms.TextBox
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents txkondisi As System.Windows.Forms.TextBox
+    Friend WithEvents txusia As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As Button
+End Class
---
+++ DSS SAW/Form1.vb
@@ -0,0 +1,18 @@
+﻿Imports MySql.Data.MySqlClient
+Public Class Form1
+    Dim cn As New MySqlConnection
+
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        cn.ConnectionString = "server=sql6.freemysqlhosting.net;user=sql6500359;password=jzTWkvE6rI;database=sql6500359;allow user variables=true"
+        cn.Open()
+        MsgBox("You are connected")
+        cn.Close()
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click, Button4.Click
+        formdata.Show()
+
+    End Sub
+End Class
